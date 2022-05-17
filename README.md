904e38c7dabe0d25a16f4ae72003578e4457b3e6
Fizzify <fizzyplayzoriginal5@gmail.com>
2022-05-17 09:07:45 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sherlock-holmes-quotes
+An API for Sherlock Holmes quotes, ranging from the book to the BBC show.
---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.DATABASE);
+
+const quoteSchema = {
+  quote: String,
+  author: String,
+};
+
+const Quote = mongoose.model("Quote", quoteSchema);
+
+app.set("view engine", "ejs");
+
+app
+  .route("/quotes")
+  .get((req, res) => {
+    Quote.find((err, foundQuotes) => {
+      if (foundQuotes) {
+        res.send(foundQuotes);
+      } else {
+        res.send("Error, no quotes found.");
+      }
+    });
+  })
+  .post((req, res) => {
+    const newQuote = new Quote({
+      quote: req.body.quote,
+      author: req.body.author,
+    });
+
+    newQuote.save((err) => {
+      if (!err) {
+        res.send("Successfully created a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+app.route("/quotes/random").get((req, res) => {
+  Quote.find((err, foundQuotes) => {
+    if (foundQuotes) {
+      const randomNumber = Math.floor(Math.random() * foundQuotes.length);
+      console.log(randomNumber);
+      res.send(foundQuotes[randomNumber]);
+    } else {
+      res.send("Error, no quotes found.");
+    }
+  });
+});
+
+app
+  .route("/quotes/:quoteContent")
+  .get((req, res) => {
+    Quote.findOne({ quote: req.params.quoteContent }, (err, foundQuote) => {
+      if (foundQuote) {
+        res.send(foundQuote);
+      } else {
+        res.send("No quote found with that quote.");
+      }
+    });
+  })
+  .put((req, res) => {
+    Quote.updateOne(
+      {
+        quote: req.params.quoteContent,
+      },
+      {
+        quote: req.body.quote,
+        author: req.body.author,
+      },
+      (err) => {
+        if (!err) {
+          res.send("Successfully updated quote.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
+
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Server started on port ${port}`));
